ee61ddf16b70da13c5e4ecaf2db05b4f58c4d8b6
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:46 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -13,9 +13,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/godsectport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/godsectport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/godsectport.git'), string(name: 'PORT_DESCRIPTION', value: 'A utility to create Go structure types from DSECT information in ADATA file created by HLASM.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/godsectport.git'), string(name: 'PORT_DESCRIPTION', value: 'A utility to create Go structure types from DSECT information in ADATA file created by HLASM.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
